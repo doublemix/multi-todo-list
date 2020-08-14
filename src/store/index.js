f9d8bf97c64ad039fd2ad7395ef8f168d60ce708
Mitchel Myers <32748075+doublemix@users.noreply.github.com>
2020-08-14 14:23:03 -0400

    Persisting state

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
@@ -10,7 +11,44 @@ const state = {
   items: {}
 };
 
-const mutations = {};
+const mutations = {
+  INCREMENT_NEXT_ID(state) {
+    state.nextId++;
+  },
+  SET_CURRENT_LIST_ID(state, { listId }) {
+    state.currentListId = listId;
+  },
+  SET_LIST(state, { listId, list }) {
+    Vue.set(state.itemLists, listId, list);
+  },
+  SET_LIST_NAME(state, { listId, name }) {
+    state.itemLists[listId].name = name;
+  },
+  INSERT_ITEM_INTO_LIST(state, { listId, index = 0, itemId }) {
+    const list = state.itemLists[listId];
+    list.items.splice(index, 0, itemId);
+  },
+  MOVE_ITEM_IN_LIST(state, { listId, fromIndex, toIndex }) {
+    const list = state.itemLists[listId];
+    const items = list.items;
+    const [value] = items.splice(fromIndex, 1);
+    items.splice(toIndex, 0, value);
+  },
+  REMOVE_ITEM_FROM_LIST(state, { listId, itemId }) {
+    const list = state.itemLists[listId];
+    list.items = list.items.filter((id) => id !== itemId);
+  },
+  SET_ITEM(state, { itemId, item }) {
+    Vue.set(state.items, itemId, item);
+  },
+  UPDATE_ITEM_TEXT(state, { itemId, text }) {
+    const item = state.items[itemId];
+    item.text = text;
+  },
+  DELETE_ITEM(state, { itemId }) {
+    Vue.delete(state.items, itemId);
+  }
+};
 
 const getters = {
   currentList(state, getters) {
@@ -32,59 +70,61 @@ const getters = {
 };
 
 const actions = {
-  getNextId({ state }) {
-    const id = state.nextId++;
+  getNextId({ commit, state }) {
+    const id = state.nextId;
+    commit("INCREMENT_NEXT_ID");
     return id;
   },
 
-  async addList({ state, dispatch }, { name = "New List" } = {}) {
+  async addList({ commit, dispatch }, { name = "New List" } = {}) {
     const id = await dispatch("getNextId");
     const newList = { id, name, items: [] };
-    Vue.set(state.itemLists, id, newList);
+    commit("SET_LIST", { listId: id, list: newList });
     return id;
   },
 
-  updateListName({ getters }, { listId, name }) {
-    const list = getters.getItemList(listId);
-    list.name = name;
+  updateListName({ commit }, { listId, name }) {
+    commit("SET_LIST_NAME", { listId, name });
   },
 
-  async addItemToList({ state, dispatch, getters }, { listId, text }) {
+  async addItemToList({ commit, dispatch }, { listId, text }) {
     const id = await dispatch("getNextId");
-    const list = getters.getItemList(listId);
     const newItem = { id, text };
-    list.items.unshift(id);
-    Vue.set(state.items, id, newItem);
+    commit("SET_ITEM", { itemId: id, item: newItem });
+    commit("INSERT_ITEM_INTO_LIST", { listId, itemId: id });
     return id;
   },
 
-  deleteItemFromList({ state, getters }, { listId, itemId }) {
-    const list = getters.getItemList(listId);
-    list.items = list.items.filter((it) => it !== itemId);
-    Vue.delete(state.items, itemId);
+  deleteItemFromList({ commit }, { listId, itemId, deleteItem = true }) {
+    commit("REMOVE_ITEM_FROM_LIST", { listId, itemId });
+    if (deleteItem) {
+      commit("DELETE_ITEM", { itemId });
+    }
   },
 
-  updateItemText({ getters }, { itemId, text }) {
-    const item = getters.getItem(itemId);
-    item.text = text;
+  updateItemText({ commit }, { itemId, text }) {
+    commit("UPDATE_ITEM_TEXT", { itemId, text });
   },
 
-  moveItemInList({ getters }, { listId, itemId, toItemId }) {
+  moveItemInList({ commit, getters }, { listId, itemId, toItemId }) {
     const list = getters.getItemList(listId);
     const items = list.items;
     const fromIndex = items.findIndex((it) => it === itemId);
     const toIndex = items.findIndex((it) => it === toItemId);
-    if (fromIndex === toIndex) return;
-    items.splice(fromIndex, 1);
-    items.splice(toIndex, 0, itemId);
+    commit("MOVE_ITEM_IN_LIST", { listId, fromIndex, toIndex });
   },
 
-  switchToList({ state }, { listId }) {
-    state.currentListId = listId;
+  switchToList({ commit }, { listId }) {
+    commit("SET_CURRENT_LIST_ID", { listId });
   }
 };
 
 export default new Vuex.Store({
+  plugins: [
+    createPersistedState({
+      key: "@multi-todo-list"
+    })
+  ],
   state,
   mutations,
   getters,
